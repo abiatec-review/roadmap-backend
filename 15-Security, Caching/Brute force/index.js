5fa4ddb588534441a615024a87e5f56b7a9ae217
Kulgeyko93 <Kulgeyko93@mail.ru>
2022-07-08 15:55:51 +0300

    feat: testing

---
+++ 15-Security, Caching/Brute force/index.js
@@ -0,0 +1,1 @@
+// https://medium.com/nuances-of-programming/%D0%BF%D1%80%D0%BE%D1%81%D1%82%D0%B0%D1%8F-%D0%B7%D0%B0%D1%89%D0%B8%D1%82%D0%B0-%D0%BE%D1%82-dos-%D0%B8-%D0%B1%D1%80%D1%83%D1%82%D1%84%D0%BE%D1%80%D1%81-%D0%B0%D1%82%D0%B0%D0%BA-53af4c33c12d
---
+++ 15-Security, Caching/Caching/index.js
@@ -0,0 +1,5 @@
+// https://www.honeybadger.io/blog/nodejs-caching/
+
+// https://www.youtube.com/watch?v=ckyxNII7UJk
+
+// https://www.npmjs.com/package/node-cache
