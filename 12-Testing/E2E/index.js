4b4fa6d266fe334923584554604df2b0d7cff5fb
yuriy17 <yuriy_sh@zoho.com>
2022-07-12 16:23:18 +0300

    add e2e article

--- 12-Testing/E2E/index.js
+++ 12-Testing/E2E/index.js
@@ -1,1 +1,4 @@
+// https://www.freecodecamp.org/news/end-point-testing/
+// https://jondot.medium.com/4-ways-to-test-your-apis-end-to-end-eb092abf1b6d
+
 // https://www.udemy.com/course/nodejs-start/learn/lecture/29115728#overview
