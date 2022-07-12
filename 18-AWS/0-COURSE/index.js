dbac674b63489185403319bc0a1b3fb94c1106c9
Kulgeyko93 <Kulgeyko93@mail.ru>
2022-07-12 18:41:32 +0300

    add tasks

---
+++ 18-AWS/0-COURSE/index.js
@@ -0,0 +1,3 @@
+// https://www.udemy.com/course-dashboard-redirect/?course_id=1921420
+
+// https://www.udemy.com/course/aws-certified-solutions-architect-associate-saa-c02/learn/lecture/13528014?start=0#overview 
