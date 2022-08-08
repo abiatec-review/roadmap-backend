2696f3388e3ff6925477c8f1f28ab18cdf7dd5a6
yuriy17 <yuriy_sh@zoho.com>
2022-08-08 11:54:51 +0300

    add streams to fs module

--- 05-basic nodejs/2-module fs/tasks/copy.js
+++ 05-basic nodejs/2-module fs/tasks/copy.js
@@ -1,31 +1,43 @@
-import { existsSync, copyFile, readdir, mkdir } from 'fs';
+import { existsSync, createReadStream, createWriteStream, readdir, mkdir } from 'fs';
 import path from 'path';
 import { __dirname } from './helper.js';
 
-export const copy = async () => {
-  const pathToFiles = path.join(__dirname, 'files');
-  const pathToFilesCopy = path.join(__dirname, 'files_copy');
+const copy = async () => {
+  try {
+    const pathToFiles = path.join(__dirname, 'files');
+    const pathToFilesCopy = path.join(__dirname, 'files_copy');
 
-  if (!existsSync(pathToFiles) && existsSync(pathToFilesCopy)) {
-    throw new Error('FS operation failed');
+    if (!existsSync(pathToFiles) && existsSync(pathToFilesCopy)) {
+      throw new Error('FS operation failed');
+    }
+    await mkdir(pathToFilesCopy, (err) => {
+      if (err) throw err;
+      console.info('mkdir operation succeed');
+    });
+    await readdir(pathToFiles, (err, files) => {
+      //handling error
+      if (err) {
+        return console.log('Unable to scan directory: ' + err);
+      }
+      if (files.length) {
+        for (let i = 0; i < files.length; i += 1) {
+          const fr = createReadStream(path.join(pathToFiles, files[i]));
+          const fw = createWriteStream(path.join(pathToFilesCopy, files[i]));
+
+          fr.on('data', (chunk) => {
+            fw.write(chunk);
+          });
+
+          // without stream
+          // copyFile(path.join(pathToFiles, files[i]), path.join(pathToFilesCopy, files[i]), (err) => {
+          //   if (err) throw err;
+          //   console.info('FS operation succeed');
+          // });
+        }
+      }
+    });
+  } catch (error) {
+    throw new Error(`can't copy - ${error}`);
   }
-  await mkdir(pathToFilesCopy, (err) => {
-    if (err) throw err;
-    console.info('mkdir operation succeed');
-  });
-  await readdir(pathToFiles, (err, files) => {
-    //handling error
-    if (err) {
-      return console.log('Unable to scan directory: ' + err);
-    }
-    //listing all files using forEach
-    for (let i = 0; i < files.length; i += 1) {
-      // Do whatever you want to do with the file
-      copyFile(path.join(pathToFiles, files[i]), path.join(pathToFilesCopy, files[i]), (err) => {
-        if (err) throw err;
-        console.info('FS operation succeed');
-      });
-    }
-  });
 };
 copy();
--- 05-basic nodejs/2-module fs/tasks/create.js
+++ 05-basic nodejs/2-module fs/tasks/create.js
@@ -3,15 +3,19 @@ import path from 'path';
 import { __dirname } from './helper.js';
 
 export const create = async () => {
-  const pathToFile = path.join(__dirname, 'files', 'fresh.txt');
+  try {
+    const pathToFile = path.join(__dirname, 'files', 'fresh.txt');
 
-  if (existsSync(pathToFile)) {
-    throw new Error('FS operation failed');
-  } else {
-    await writeFile(pathToFile, 'I am fresh and young', (err) => {
-      if (err) throw err;
-      console.info('FS operation succeed');
-    });
+    if (existsSync(pathToFile)) {
+      throw new Error('FS operation failed');
+    } else {
+      await writeFile(pathToFile, 'I am fresh and young', (err) => {
+        if (err) throw err;
+        console.info('FS operation succeed');
+      });
+    }
+  } catch (error) {
+    throw new Error(`can't create - ${error}`);
   }
 };
 
