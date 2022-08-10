e6e4570e098949f7af0eb0259899907be18357f9
yuriy17 <yuriy_sh@zoho.com>
2022-08-10 17:06:45 +0300

    add zlib tasks

--- 05-basic nodejs/3-module zlib/tasks/compress.js
+++ 05-basic nodejs/3-module zlib/tasks/compress.js
@@ -1,4 +1,26 @@
+import zlib from 'zlib';
+import path from 'path';
+import { createReadStream, createWriteStream, existsSync } from 'fs';
+import { __dirname } from './helper.js';
 
-export const compress = async () => {};
+export const compress = async () => {
+  try {
+    const pathToFile = path.join(__dirname, 'files', 'fileToCompress.txt');
 
-compress()
+    if (existsSync(pathToFile)) {
+      const gzip = zlib.createGzip();
+
+      const read = createReadStream(pathToFile);
+      const write = createWriteStream(path.join(__dirname, 'files', 'archive.gz'));
+      //Transform stream which is zipping the input file
+      read.pipe(gzip).pipe(write);
+      console.log('Zipped Successfully');
+    } else {
+      throw new Error(`ZLIB operation failed  - wrong path or file doesn't exist`);
+    }
+  } catch (error) {
+    throw new Error(`can't compress - ${error}`);
+  }
+};
+
+compress();
--- 05-basic nodejs/3-module zlib/tasks/decompress.js
+++ 05-basic nodejs/3-module zlib/tasks/decompress.js
@@ -1,4 +1,26 @@
+import zlib from 'zlib';
+import path from 'path';
+import { createReadStream, createWriteStream, existsSync } from 'fs';
+import { __dirname } from './helper.js';
 
-export const decompress = async () => {};
+export const decompress = async () => {
+  try {
+    const pathToFile = path.join(__dirname, 'files', 'archive.gz');
 
-decompress()
+    if (existsSync(pathToFile)) {
+      const unzip = zlib.createUnzip();
+
+      const read = createReadStream(pathToFile);
+      const write = createWriteStream(path.join(__dirname, 'files', 'fileToCompress.txt'));
+      //Transform stream which is zipping the input file
+      read.pipe(unzip).pipe(write);
+      console.log('Zipped Successfully');
+    } else {
+      throw new Error(`ZLIB operation failed  - wrong path or file doesn't exist`);
+    }
+  } catch (error) {
+    throw new Error(`can't compress - ${error}`);
+  }
+};
+
+decompress();
