c0d7bc9108925a3a00c2b60aa94d8c73ddbd0b5d
yuriy17 <yuriy_sh@zoho.com>
2022-08-15 15:28:44 +0300

    fix udemy links in child process

--- 05-basic nodejs/8-module child-process/index.js
+++ 05-basic nodejs/8-module child-process/index.js
@@ -1,9 +1,8 @@
-// https://www.udemy.com/course/nodejs-start/learn/lecture/28324090#overview
-
+// https://www.udemy.com/course/nodejs-start/learn/lecture/28330196#overview
+// https://www.udemy.com/course/nodejs-start/learn/lecture/28420392#overview
 
 // https://nodejsdev.ru/api/process/
 
-
-// docs 
+// docs
 // http2
 // https://nodejs.org/dist/latest-v16.x/docs/api/child_process.html
