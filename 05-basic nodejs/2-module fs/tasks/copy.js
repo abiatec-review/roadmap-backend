c67f4da0fbc6409921039d45a7b056e43f438428
isolonenko <103060679+isolonenko@users.noreply.github.com>
2022-10-17 11:32:23 +0300

    Copy func implemented

--- 05-basic nodejs/2-module fs/tasks/copy.js
+++ 05-basic nodejs/2-module fs/tasks/copy.js
@@ -1,2 +1,27 @@
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export const copy = async () => {};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const fileToCopy = path.join(__dirname, "/files", "dontLookAtMe.txt");
+
+export const copy = async () => {
+  try {
+    await fs.copyFile(
+      fileToCopy,
+      path.join(__dirname, "/files", "copiedFile.txt")
+    );
+
+    const readFile = await fs.readFile(path.join(__dirname, "/files", "copiedFile.txt"), {
+      encoding: "utf8",
+    });
+
+    readFile && console.log("Contents:", readFile)
+  } catch (e) {
+    console.log('Something went wrong')
+    throw e;
+  }
+};
+
+copy();
