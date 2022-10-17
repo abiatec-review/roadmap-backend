6f178897d15da4fb69585e9c2584b560dacace24
isolonenko <ivan.solonenko@corsearch.com>
2022-10-17 19:11:27 +0300

    create func

--- 05-basic nodejs/2-module fs/tasks/create.js
+++ 05-basic nodejs/2-module fs/tasks/create.js
@@ -1,2 +1,29 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export const create = async () => {};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const create = async () => {
+  try {
+    const isFileExists = fs.existsSync(
+      path.join(__dirname, "/files", "/fresh.txt")
+    );
+
+    if (!isFileExists) {
+      fs.writeFile(
+        path.join(__dirname, "/files", "/fresh.txt"),
+        "I am fresh and young",
+        { encoding: "utf-8" },
+        () => console.log("File created")
+      );
+    }
+
+    console.log("File already exists");
+  } catch (e) {
+    throw e;
+  }
+};
+
+create();
