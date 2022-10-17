92dc8929e3ddbc6a80d5ae83c7a4e61e5a12e3c2
isolonenko <ivan.solonenko@corsearch.com>
2022-10-17 19:22:51 +0300

    rename func

--- 05-basic nodejs/2-module fs/tasks/rename.js
+++ 05-basic nodejs/2-module fs/tasks/rename.js
@@ -1,2 +1,27 @@
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
 
-export const rename = async () => {};
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const rename = async () => {
+  try {
+    const isFileExists =
+      !fs.existsSync(path.join(__dirname, "/files", "/wrongFilename.txt")) ||
+      fs.existsSync(path.join(__dirname, "/files", "/properFilename.md"));
+
+    if (isFileExists) {
+      throw new Error("FS operation failed");
+    }
+
+    await fs.promises.rename(
+      path.join(__dirname, "/files", "/wrongFilename.txt"),
+      path.join(__dirname, "/files", "/properFilename.md")
+    );
+  } catch (e) {
+    throw e;
+  }
+};
+
+rename();
