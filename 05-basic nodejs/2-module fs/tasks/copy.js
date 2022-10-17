14e936a5d5e0fa99f84bf055cb7fcb2a02e5bdf5
isolonenko <103060679+isolonenko@users.noreply.github.com>
2022-10-17 11:40:30 +0300

    Have read description, implemented copy folder func

--- 05-basic nodejs/2-module fs/tasks/copy.js
+++ 05-basic nodejs/2-module fs/tasks/copy.js
@@ -4,22 +4,21 @@ import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const fileToCopy = path.join(__dirname, "/files", "dontLookAtMe.txt");
+const folderToCopy = path.join(__dirname, "/files");
 
 export const copy = async () => {
   try {
-    await fs.copyFile(
-      fileToCopy,
-      path.join(__dirname, "/files", "copiedFile.txt")
-    );
+    await fs.cp(folderToCopy, path.join(__dirname, "/files_copy"), {
+      recursive: true,
+    });
 
-    const readFile = await fs.readFile(path.join(__dirname, "/files", "copiedFile.txt"), {
+    const readFile = await fs.readdir(path.join(__dirname, "/files_copy"), {
       encoding: "utf8",
     });
 
-    readFile && console.log("Contents:", readFile)
+    readFile && console.log("Contents:", readFile);
   } catch (e) {
-    console.log('Something went wrong')
+    console.log("Something went wrong");
     throw e;
   }
 };
