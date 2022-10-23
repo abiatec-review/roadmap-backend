e332ba7a46505e511dd84a29039f839f31f87931
Kulgeyko93 <Kulgeyko93@mail.ru>
2022-10-23 20:34:12 +0300

    add tasks

---
+++ 09-Patterns&algoritms/algorithm/tasks/showCallLast2Min.js
@@ -0,0 +1,14 @@
+/*
+  Задача:
+
+  Implement the following class. The solution will be evaluated based on your choice of data structures and
+  the algorithmic efficiency, so think about how your solution will behave as the frequency of calls to accept
+  and mean grows. 
+
+  Keep in mind that a functional solution may not be efficient. To pass this screening exercise, 
+  you need to implement a correct and efficient solution.
+
+  Придумать систему, в которой
+  1 функция периодически вызываться для 
+  2 функция будет выдавать числа, которые которые были потреблены за последние 2 минуты. 
+*/
